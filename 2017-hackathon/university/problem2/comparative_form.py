e0c243e883d6101da86dbb252f6b3ed1983f5ef4
Melkisedek Hamakali <melkisedek@users.noreply.github.com>
2018-01-08 23:02:13 +0200

    Fix grammar

--- 2017-hackathon/university/problem2/comparative_form.py
+++ 2017-hackathon/university/problem2/comparative_form.py
@@ -1,5 +1,5 @@
-# Note: this program will fail not give the correct output when 
-# when irregular adjectives such as 'bad','good' or those that
+# Note: This program will not give the correct output when 
+# given irregular adjectives such as 'bad','good' or those that
 # require the last letter to be repeated such as 'fat'.
 with open('sample_input.txt') as sample_input:
     lines = sample_input.readlines() # read all lines in the file
@@ -8,7 +8,7 @@ with open('sample_input.txt') as sample_input:
         # line ends in 'est', don't modify
         if line[-3:] == 'est':
             print(line)
-        # line ends in 'er', replace with 'est
+        # line ends in 'er', replace 'er' with 'est
         elif line[-2:] == 'er':
             line = line[:-2] + 'est' # observe the position of the ':'
             print(line)
